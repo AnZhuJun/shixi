3cfb67da5118dcb9536f0a6864cb0c44a7c77175
AnZhuJun <a17809283667@163.com>
2019-09-16 14:34:52 +0800

    init

---
+++ src/main/java/com/cs/tobaccosystem/TobaccosystemApplication.java
@@ -0,0 +1,13 @@
+package com.cs.tobaccosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobaccosystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobaccosystemApplication.class, args);
+    }
+
+}
