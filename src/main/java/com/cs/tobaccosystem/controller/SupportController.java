a5a1d3492da37eb233d136e003835a1112eb7f42
AnZhuJun <a17809283667@163.com>
2019-09-16 16:11:37 +0800

    tobacco

--- src/main/java/com/cs/tobaccosystem/controller/SupportController.java
+++ src/main/java/com/cs/tobaccosystem/controller/SupportController.java
@@ -3,9 +3,7 @@ package com.cs.tobaccosystem.controller;
 import com.cs.tobaccosystem.bean.Support;
 import com.cs.tobaccosystem.service.SupportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,31 @@ public class SupportController {
     @GetMapping("/all")
     public List<Support> getAllSupport(){
         return supportService.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Support getById(@PathVariable int id){
+        return supportService.findByID(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public Support getByName(@PathVariable String name){
+        return supportService.findByName(name);
+    }
+
+    @PostMapping("/update/{id}")
+    public Support update(@PathVariable int id,@RequestBody Support support){
+        support.setSupportid(id);
+        return supportService.updateSupport(support);
+    }
+
+    @PutMapping("/add")
+    public Support add(@RequestBody Support support){
+        return supportService.addSupport(support);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Support delete(@PathVariable int id){
+        return supportService.deleteSupportById(id);
     }
 }
---
+++ src/main/java/com/cs/tobaccosystem/controller/TobaccoController.java
@@ -0,0 +1,52 @@
+package com.cs.tobaccosystem.controller;
+
+import com.cs.tobaccosystem.bean.Tobacco;
+import com.cs.tobaccosystem.dao.TobaccoDao;
+import com.cs.tobaccosystem.service.TobaccoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tobacco")
+public class TobaccoController {
+    @Autowired
+    TobaccoService tobaccoService;
+
+    @GetMapping("/all")
+    public List<Tobacco> getAllTobacco(){
+        return tobaccoService.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Tobacco getById(@PathVariable int id){
+        return tobaccoService.findById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public Tobacco getByName(@PathVariable String name){
+        return tobaccoService.findByName(name);
+    }
+
+    @GetMapping("/supportid/{id}")
+    public List<Tobacco> getBySupportId(@PathVariable int id){
+        return tobaccoService.findBySupportId(id);
+    }
+
+    @PostMapping("/update/{id}")
+    public Tobacco update(@PathVariable int id,@RequestBody Tobacco tobacco){
+        tobacco.setTobaccoid(id);
+        return tobaccoService.updateTobacco(tobacco);
+    }
+
+    @PutMapping("/add")
+    public Tobacco add(@RequestBody Tobacco tobacco){
+        return tobaccoService.addTobacco(tobacco);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Tobacco delete(@PathVariable int id){
+        return tobaccoService.deleteById(id);
+    }
+}
